113115b76e84fd050fb3c88bac5f8ed34152a408
bnechyporenko <bogdannechiporenko@gmail.com>
2022-01-12 12:54:33 +0100

    ENG-8769 Make certain docker variables configurable from operator

---
+++ build_operator.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+containerOrganization="xldevdocker"
+branchName="master"
+
+if [[ $# -eq 0 ]] ; then
+    printf "\e[31mProvide in a first parameter a version (SemVer compatible) to release !\e[m\n"
+    echo "For example:"
+    printf "\e[1;32m./build_operator.sh 0.0.1-openshift \e[0m"
+    echo ""
+    printf "\e[1;32m./build_operator.sh 0.0.1-openshift acierto\e[0m"
+    echo ""
+    printf "\e[1;32m./build_operator.sh 0.0.1-openshift acierto ENG-8769\e[0m"
+    echo ""
+    printf "Second example is about how to push the image to a non-default organization"
+    echo ""
+    printf "Third example shows how to push from the branch, even if you want to use the default organization, for a non-default branch you have to specify name for an organization."
+    echo ""
+    exit 1
+fi
+
+if [[ $# > 1 ]] ; then
+  containerOrganization=$2
+fi
+
+if [[ $# > 2 ]] ; then
+  branchName=$3
+fi
+
+mkdir xlr
+cd xlr
+git clone git@github.com:xebialabs/xl-release-kubernetes-helm-chart.git -b $branchName
+cd xl-release-kubernetes-helm-chart
+rm -f values-haproxy.yaml
+mv values-nginx.yaml values.yaml
+helm dependency update .
+rm -f Chart.lock
+cd ..
+helm package xl-release-kubernetes-helm-chart
+rm -rf xl-release-kubernetes-helm-chart
+mv digitalai-release-*.tgz xlr.tgz
+operator-sdk init --domain digital.ai --plugins=helm
+operator-sdk create api --group=xlr --version=v1alpha1 --helm-chart=xlr.tgz
+export OPERATOR_IMG="docker.io/$containerOrganization/release-operator:$1"
+make docker-build docker-push IMG=$OPERATOR_IMG
+cd ..
+rm -rf xlr
